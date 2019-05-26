fc11e016b9ab64d28850627404d210867c3bc9f5
Vipul Kumar Singh <vipul24.9669@gmail.com>
2019-05-26 19:52:13 +0530

    Added CSS

--- src/main/java/com/vkstech/springboot/thymeleafdemo/controller/DemoController.java
+++ src/main/java/com/vkstech/springboot/thymeleafdemo/controller/DemoController.java
@@ -1,10 +1,10 @@
 package com.vkstech.springboot.thymeleafdemo.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Date;
 
 @Controller
 public class DemoController {
