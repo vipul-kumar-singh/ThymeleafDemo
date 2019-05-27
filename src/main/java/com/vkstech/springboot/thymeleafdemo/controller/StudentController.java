4220fb1c6180ab0b7d2714edf0bef60791a30076
Vipul Kumar Singh <vipul24.9669@gmail.com>
2019-05-28 00:02:02 +0530

    CRUD operation - Add Student

--- src/main/java/com/vkstech/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/vkstech/springboot/thymeleafdemo/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.vkstech.springboot.thymeleafdemo.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,12 +17,12 @@ public class StudentController {
 
     private StudentService studentService;
 
-    public StudentController(StudentService studentService){
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping("/list")
-    public String listStudents(Model model){
+    public String listStudents(Model model) {
 
         // get employees from db
         List<Student> studentList = studentService.findAll();
@@ -28,11 +30,11 @@ public class StudentController {
         // add to the spring model
         model.addAttribute("students", studentList);
 
-        return "list-students";
+        return "students/list-students";
     }
 
     @GetMapping("/list/bootstrap")
-    public String listStudentsBootstrap(Model model){
+    public String listStudentsBootstrap(Model model) {
 
         // get employees from db
         List<Student> studentList = studentService.findAll();
@@ -40,6 +42,26 @@ public class StudentController {
         // add to the spring model
         model.addAttribute("students", studentList);
 
-        return "list-students-bootstrap";
+        return "students/list-students-bootstrap";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        //create model attribute to bind form data
+        Student student = new Student();
+
+        model.addAttribute("student", student);
+        return "students/student-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("student") Student student){
+
+        //save the student
+        studentService.save(student);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/students/list/bootstrap";
     }
 }
